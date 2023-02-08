1790a63357886a53da0a168bb26a0d29a35c5b8b
Vijay Krishnavanshi <vijaykrishnavanshi@gmail.com>
2023-02-08 13:20:39 +0530

    Added S3 persistance to gun

--- examples/http.js
+++ examples/http.js
@@ -19,7 +19,15 @@
 		config.server = require('http').createServer(Gun.serve(__dirname));
 	}
 
-	var gun = Gun({web: config.server.listen(config.port), peers: config.peers});
+	var gun = Gun({
+		web: config.server.listen(config.port),
+		peers: config.peers,
+		s3: {
+			key: process.env.AWS_ACCESS_KEY_ID, // AWS Access Key
+			secret: process.env.AWS_SECRET_ACCESS_KEY, // AWS Secret Token
+			bucket: process.env.AWS_S3_BUCKET, // The bucket you want to save into
+		}
+	});
 
 	console.log('Relay peer started on port ' + config.port + ' with /gun');
 
